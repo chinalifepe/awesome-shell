36d90f0d0593e0d511f7094f48838f6f93150b44
liuxyCN <lxy@live.cn>
2019-04-07 17:25:33 +0800

    fix bug & code style

--- mountdisk.sh
+++ mountdisk.sh
@@ -4,32 +4,40 @@ fdisk -l | grep -o "^磁盘 /dev/[sh]d[a-z]"
 
 read -p "请输入需要挂载的磁盘: " choice
 echo "输入q或Q退出程序"
-until [ $choice != 'q' -a $choice != 'Q' -a $choice != 'quit' -a $choice != 'Quit' ]&& fdisk -l | grep -o "^Disk /dev/[sh]d[a-z]"|grep $choice;do
-if [ $choice == 'q' -o $choice == 'Q' -o $choice == 'quit' -o $choice == 'Quit' ];then  echo "程序已退出"
-exit 5
-else
-echo "输入错误"
-read -p "请输入需要挂载的磁盘: " choice
-fi
+
+until [ $choice != 'q' -a $choice != 'Q' -a $choice != 'quit' -a $choice != 'Quit' ]&& fdisk -l | grep -o "^磁盘 /dev/[sh]d[a-z]"|grep $choice; 
+do
+	if [ $choice == 'q' -o $choice == 'Q' -o $choice == 'quit' -o $choice == 'Quit' ]; 
+	then 
+		echo "程序已退出"
+		exit 5
+	else
+		echo "输入错误"
+		read -p "请输入需要挂载的磁盘: " choice
+	fi
 done
+
 echo "磁盘 $choice 即将初始化分区，请确认"
 read -p "(y or n): " choiceyn
-if [ $choiceyn == 'N' -o $choiceyn == 'n' ];then
 
-echo "程序已退出"
-exit 5
-elif [ $choiceyn == 'Y' -o $choiceyn == 'y' ];then
+if [ $choiceyn == 'N' -o $choiceyn == 'n' ];
+then
+	echo "程序已退出"
+	exit 5
+elif [ $choiceyn == 'Y' -o $choiceyn == 'y' ];
+then
+	echo "正在初始化....请稍后"
+	dd if=/dev/zero of=$choice bs=512 count=1 &>/dev/null
 
-echo "正在初始化....请稍后"
-dd if=/dev/zero of=$choice bs=512 count=1 &>/dev/null
-echo "n
-p
-1
+	echo "n
+	p
+	1
 
 
-p
-w" | fdisk $choice &>/dev/null
+	p
+	w" | fdisk $choice &>/dev/null
 fi
+
 partprobe $choice
 sync
 sleep 3
