fc74deb21a8d0437d0e4e0344345cd2dfe210cc2
liuxyCN <lxy@live.cn>
2019-04-07 17:35:54 +0800

    增加 挂载第二块硬盘无交互脚本

---
+++ mount2nddisk.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dd if=/dev/zero of=/dev/sdb bs=512 count=1 &>/dev/null
+
+echo "n
+p
+1
+
+
+p
+w" | fdisk /dev/sdb &>/dev/null
+
+
+partprobe /dev/sdb
+sync
+sleep 3
+
+mkfs -t ext4 /dev/sdb1 &>/dev/null
+mkdir /data &>/dev/null
+mount /dev/sdb1 /data
+echo "已完成"
